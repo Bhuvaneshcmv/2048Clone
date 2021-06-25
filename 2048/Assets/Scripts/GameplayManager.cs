88df05b5b230efad1f8994ad0c5afd38dbb1ab5b
Bhuvanesh <bhuvaneshcmv@gmail.com>
2021-06-25 22:18:36 +0530

    Added new Input system

--- 2048/Assets/Scripts/GameplayManager.cs
+++ 2048/Assets/Scripts/GameplayManager.cs
@@ -1,20 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MoveDir
+{
+    left,
+    right,
+    up,
+    down
+}
 public class GameplayManager : MonoBehaviour
 {
     [SerializeField] private Slot _slot;
     [SerializeField] private List<Slot> _slots = new List<Slot>();
+    [SerializeField] private List<Slot> filledSlots = new List<Slot>();
     [SerializeField] private GameObject _slotsParent;
     [SerializeField] private GameObject slotsRow;
     [SerializeField] private LevelData _levelData;
     [SerializeField] private Tile _tile;
-
+    [SerializeField] private GameObject _emptyTilesParent;
+    public static Action<MoveDir> move;
     void Start()
     {
         CreatingSlots();
         CreateTiles();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
     }
 
     void CreatingSlots()
@@ -27,10 +52,10 @@ public class GameplayManager : MonoBehaviour
             tempRow = Instantiate(slotsRow, _slotsParent.transform);
             for (int j = 0; j < _levelData.slotsColumnCount; j++)
             {
-                index++;
                 tempSlot = Instantiate(_slot, tempRow.transform);
                 tempSlot.index = index;
                 _slots.Add(tempSlot);
+                index++;
             }
         }
         tempRow = null;
@@ -38,15 +63,33 @@ public class GameplayManager : MonoBehaviour
 
     void CreateTiles()
     {
-        List<int> slotsToFill = _levelData.GetInitialSlotIndices(2);
-        List<int> initialTiles = _levelData.GetInitialTileValues(2);
+        List<int> slotsToFillIds = _levelData.GetInitialSlotIndices(2);
+        List<int> initialTilesVals = _levelData.GetInitialTileValues(2);
        
         
-        Tile temp;
-        for (int i=0; i < initialTiles.Count;i++)
+        Tile tempTile;
+        for (int i=0; i < initialTilesVals.Count;i++)
         {
-            temp = Instantiate(_tile, _slots[slotsToFill[i]].transform);
-            temp.SetValue(initialTiles[i]);
+            tempTile = Instantiate(_tile);
+            tempTile.SetValue(initialTilesVals[i]);
+            _slots[slotsToFillIds[i]].placeTile(tempTile);
+        }
+    }
+
+    void MoveTile(int presentSlotId, int futureSlotId)
+    {
+        Tile tempTile;
+        tempTile = _slots[presentSlotId].RemoveTile();
+        tempTile.transform.SetParent(_emptyTilesParent.transform);
+        _slots[futureSlotId].placeTile(tempTile);
+    }
+
+    void TileMovementHandler(MoveDir movementDirection)
+    {
+        switch (movementDirection)
+        {
+            case MoveDir.left:
+                break;
         }
     }
 }
--- 2048/Assets/Scripts/Slot.cs
+++ 2048/Assets/Scripts/Slot.cs
@@ -1,8 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
     public int index { get; set;}
+    public Tile tileWithin;
+
+    public void placeTile(Tile tile)
+    {
+        tile.transform.position = transform.position;
+        tile.transform.SetParent(transform);
+        tileWithin = tile;
+    }
+
+    public Tile RemoveTile()
+    {
+        Tile tempTile = tileWithin;
+        tileWithin.transform.SetParent(null);
+        tileWithin = null;
+        return tempTile;
+    }
 }
